d55d155c5871fea38036ac569abec47119c3c272
Tux Ubuntu <tuxubuntu@mail.ru>
2015-10-31 21:47:38 +0300

    fix tmp dir

--- files/restore/.inless/express.js
+++ files/restore/.inless/express.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(multer({
-	dest: path.resolve('./../', servConfig.temp || './tmp/')
+	dest: path.resolve(servConfig.temp || './tmp/')
 }));
 app.use(express.static(path.join(__dirname, './../application/static')));
 app.use(cookieParser);
